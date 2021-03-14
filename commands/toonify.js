07c2539157f98b6b46140f21fd53abb50ff8fca7
kikiixyz <73013580+kikiixyz@users.noreply.github.com>
2021-03-14 18:41:51 +0700

    Update toonify.js

--- commands/toonify.js
+++ commands/toonify.js
@@ -3,6 +3,7 @@ const { text, extendedText, contact, location, liveLocation, image, video, stick
 const axios = require("axios")
 const istimer = (ts) => require('moment-timezone').duration(moment() - moment(ts * 1000)).asSeconds()
 const deepai = require('deepai');
+const ffmpeg = require("ffmpeg")
 deepai.setApiKey('4ec4c7f4-63cd-457f-b244-7e12bba7ebde');
 exports.run = (bot, message, args, from, id) => {
     try {
