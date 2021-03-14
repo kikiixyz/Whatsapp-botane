a6684c2f618d0eed00262e707c130ce657ee8894
kikiixyz <73013580+kikiixyz@users.noreply.github.com>
2021-03-14 21:40:58 +0700

    Update toonify.js

--- commands/toonify.js
+++ commands/toonify.js
@@ -1,16 +1,17 @@
-const { MessageType } = require("@adiwajshing/baileys")
+const { MessageType, Mimetype} = require("@adiwajshing/baileys")
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 const axios = require("axios")
+const fs = require("fs")
+const ffmpeg = require("fluent-ffmpeg")
 const istimer = (ts) => require('moment-timezone').duration(moment() - moment(ts * 1000)).asSeconds()
 const deepai = require('deepai');
-const ffmpeg = require("ffmpeg")
 deepai.setApiKey('4ec4c7f4-63cd-457f-b244-7e12bba7ebde');
-exports.run = (bot, message, args, from, id) => {
+exports.run = async (bot, message, args, from, id) => {
     try {
 		const medianye = await JSON.parse(JSON.stringify(message).replace('quotedM','m')).message.extendedTextMessage.contextInfo
         console.log(medianye)
-		const mediaData = await conn.downloadAndSaveMediaMessage(medianye)
-		 conn.sendMessage(from, 'permintaan anda sedang di proses om', text, {quoted: message})
+		const mediaData = await bot.downloadAndSaveMediaMessage(medianye)
+		 bot.sendMessage(from, 'permintaan anda sedang di proses om', text, {quoted: message})
 		 ffmpeg(mediaData)
             .save('output.jpg')
             .on('end', async () => {
@@ -25,13 +26,20 @@ exports.run = (bot, message, args, from, id) => {
           url: respoimage,
           responseType: 'arraybuffer'
         })
-                await conn.sendMessage(toId, response.data, image, {mimetype: Mimetype.jpg, caption: 'nih om'})
+                await bot.sendMessage(from, response.data, image, {mimetype: Mimetype.jpg, caption: 'nih om'})
 
             });
 		} catch(err) {
 			await bot.sendMessage(from, 'Error!', text, {quoted: message})
 		}
 		}
+exports.help = {
+    name: "toonify",
+    description: "MEMBUAT WAJAH ANDA JADI ANEH",
+    usage: "toonify",
+    cooldown: 5
+};
+
 exports.help = {
     name: "toonify",
     description: "MEMBUAT WAJAH ANDA JADI ANEH",
