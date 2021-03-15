757206ccbee12f1dc8fe3b8f78ec71fbf46a617e
kikiixyz <73013580+kikiixyz@users.noreply.github.com>
2021-03-15 12:39:36 +0700

    ytsearch

---
+++ commands/ytsearch.js
@@ -0,0 +1,21 @@
+const { evaluate } = require("mathjs");
+const fs = require("fs-extra")
+const yts = require("yt-search")
+const { MessageType } = require("@adiwajshing/baileys")
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+
+exports.run = async (bot, message, args, body,  from) => {
+  const ms = body.slice(11)
+	var irama = await yts(ms)
+	var yutup = '';
+        irama.all.map((video) => {
+            yutup += '> ' + video.title + ' - ' + video.url + '\n\n'
+        });
+	await bot.sendMessage(from, yutup, MessageType.text, {quoted: message})}
+
+exports.help = {
+    name: "ytsearch",
+    description: "yts",
+    usage: "ytsearch < txt >",
+    cooldown: 5,
+};
