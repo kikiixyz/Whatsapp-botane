ca4a25539d5f03147c98ceb2748c6df343f7942b
kikiixyz <73013580+kikiixyz@users.noreply.github.com>
2021-03-15 18:50:58 +0700

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -5,7 +5,7 @@ const { exec } = require("child_process")
 const { MessageType } = require("@adiwajshing/baileys")
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 
-exports.run = async (bot, message, args, from) => {
+exports.run = async (bot, message, args, body, from) => {
     const image = await bot.downloadAndSaveMediaMessage(message)
     exec('cwebp -q 50 ' + image + ' -o trash/' + time + '.webp', (error, stdout, stderr) => {
         let result = fs.readFileSync('trash/' + time + '.webp')
