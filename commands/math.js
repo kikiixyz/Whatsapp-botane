364c30c5a4cb77c209ef90280775de2ca27a72c4
kikiixyz <73013580+kikiixyz@users.noreply.github.com>
2021-03-15 18:49:36 +0700

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -3,7 +3,7 @@ const { evaluate } = require("mathjs");
 const { MessageType } = require("@adiwajshing/baileys")
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 
-exports.run = async (bot, message, args, from) => {
+exports.run = async (bot, message, args, body, from) => {
     const expressions = args.join` `
     const answer = evaluate(expressions);
     bot.sendMessage(from, answer.toString(), text, { quoted: message });
